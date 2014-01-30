7297ad07dff7c2e571c3ae4087afe6de457178dd
kedruwsky <kedar.waval@gmail.com>
2014-01-30 12:54:31 +0530

    Update mapper_post_length.py

--- mapper_post_length.py
+++ mapper_post_length.py
@@ -23,13 +23,12 @@ import csv
 #-----------------------------------------------------------#
 
 reader = csv.reader(sys.stdin, delimiter='\t')
-writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
 headerSkipped = False
 
 for line in reader:
 
     if len(line) != 19:
-       print("faulty record")
+       continue
 
     if not headerSkipped:      
        headerSkipped = True    
