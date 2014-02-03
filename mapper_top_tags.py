fa8abe382eba249b18338de325d839beab47b7d0
Kedarnath Waval <kedruwsky@users.noreply.github.com>
2014-02-03 16:57:44 +0530

    Update mapper_top_tags.py

--- mapper_top_tags.py
+++ mapper_top_tags.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import csv
+import operator
 
 #-----------------------------------------------------------#
 # The mapper reads through all the records in the input,    #
@@ -8,9 +9,11 @@ import csv
 #                                                           #
 # tagnames is the 3rd field in a record.                    #
 # The mapper separates all the tags in tagnames and         #
-# outputs one tag per line.                                 #
+# outputs topN tags.                                        #
 #-----------------------------------------------------------#
+n = 10
 
+tagOccurence = {}
 reader = csv.reader(sys.stdin, delimiter='\t')
 headerSkipped = False
 
@@ -25,4 +28,13 @@ for line in reader:
 
     for i in line[2].split(" "):
         if i != '':
-           print i
+           if i not in tagOccurence:
+              tagOccurence[i] = 1
+           else:
+              tagOccurence[i] = tagOccurence[i]+1
+
+tagOccurence1 = [(v, k) for k, v in tagOccurence.items()]
+tagOccurence1 = sorted(tagOccurence1, reverse=True, key=lambda x: int(x[0]))[:n]
+
+for i in tagOccurence1:
+    print "{0}\t{1}".format(i[1], i[0])
